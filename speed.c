445aab6f62da4ca904b2372096057b5a84864089
munvoseli <muslinmelody@gmail.com>
2022-08-01 18:38:26 -0400

    another extended euclidian

--- speed.c
+++ speed.c
@@ -43,21 +43,34 @@ u32 mmi_euler(u32 n) {
 
 // https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
 u32 mmi_exteucl(u32 n) {
-	u32 rowa[4], rowb[4]; // qrst
-	rowa[1] = 1 << (BITS-1); rowa[2] = 1; rowa[3] = 0;
-	rowb[1] =             n; rowb[2] = 0; rowb[3] = 1;
+	u32 rowa[2], rowb[2]; // qrst
+	rowa[0] = 1 << (BITS-1); rowa[1] = 0;
+	rowb[0] =             n; rowb[1] = 1;
 	u32* rowx = rowa, *rowy = rowb;
-	while (rowx[1] && rowy[1]) {
-		u32 q = rowx[1] / rowy[1];
-		u32 r = rowx[1] % rowy[1];
-		u32 s = rowx[2] - q * rowy[2];
-		u32 t = rowx[3] - q * rowy[3];
-		rowx[0] = q; rowx[1] = r; rowx[2] = s; rowx[3] = t;
+	while (rowy[0]) {
+		u32 r = rowx[0] % rowy[0];
+		rowx[1] -= rowx[0] / rowy[0] * rowy[1];
+		rowx[0] = r;
 		u32* h = rowx;
 		rowx = rowy;
 		rowy = h;
 	}
-	return rowx[3];
+	return rowx[1];
+}
+
+// https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Simple_algebraic_field_extensions
+u32 mmi_eucl2(u32 n) {
+	u32 t2 = 0; u32 t1 = 1;
+	u32 r2 = 1 << (BITS-1);
+	u32 r1 = n;
+	while (r1) {
+		u32 q = r2 / r1;
+		u32 r0 = r2 - q * r1;
+		r2 = r1; r1 = r0;
+		u32 t0 = t2 - q * t1;
+		t2 = t1; t1 = t0;
+	}
+	return t2;
 }
 
 void test_val(u32 n) {
@@ -65,6 +78,7 @@ void test_val(u32 n) {
 	printf("%8x mnv if\n", mmi_mnv_if(n));
 	printf("%8x euler\n", mmi_euler(n));
 	printf("%8x extended euclidian\n", mmi_exteucl(n));
+	printf("%8x extended euclidian 2\n", mmi_eucl2(n));
 }
 
 #define TEST_FUNC(func, str) start = clock();\
@@ -82,11 +96,12 @@ int main(int argc, char** argv) {
 	u32 gers;
 	volatile u32 *pog = &gers;
 
-	for (int j = 0; j < 3; ++j) {
-		TEST_FUNC(mmi_mnv_noif, "mnv noif")
+	for (int j = 0; j < 1; ++j) {
 		TEST_FUNC(mmi_mnv_if, "mnv if")
-		TEST_FUNC(mmi_euler, "euler")
 		TEST_FUNC(mmi_exteucl, "extended euclidian")
+		TEST_FUNC(mmi_eucl2, "extended euclidian 2")
+		TEST_FUNC(mmi_mnv_noif, "mnv noif")
+		TEST_FUNC(mmi_euler, "euler")
 	}
 
 	return 0;
