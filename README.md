cfea87cabf8c92b5b97a448ed7670976eb69cf69
munvoseli <muslinmelody@gmail.com>
2022-08-01 17:48:42 -0400

    add extended euclidian

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ The mnv algorithms are a thing.
 
 I assume the Euler algorithm is how it's implemented elsewhere, but I did re-create it from Euler's theorem with the totient function.
 
-I haven't figured out the Extended Euclidian algorithm yet, so it is unimplemented.
+Extended Euclidian is also there now.
 
 <hr/>
 
--- speed.c
+++ speed.c
@@ -3,12 +3,13 @@
 
 typedef unsigned int u32;
 #define TRIALS 10000000
+#define BITS 32
 
 u32 mmi_mnv_noif(u32 n) {
-	int s = n;
-	int m = 1;
-	for (int i = 1; i < 32; ++i) {
-		int a = s & (1 << i);
+	u32 s = n;
+	u32 m = 1;
+	for (int i = 1; i < BITS; ++i) {
+		u32 a = s & (1 << i);
 		s += n * a;
 		m |= a;
 	}
@@ -18,7 +19,7 @@ u32 mmi_mnv_noif(u32 n) {
 u32 mmi_mnv_if(u32 n) {
 	int s = n;
 	int m = 1;
-	for (int i = 1; i < 32; ++i) {
+	for (int i = 1; i < BITS; ++i) {
 		int a = s & (1 << i);
 		if (a) {
 			s += n << i;
@@ -32,7 +33,7 @@ u32 mmi_euler(u32 n) {
 	u32 a = n;
 	u32 b = n;
 	//for (; a != 1;) {
-	for (int i = 0; i < 32; ++i) {
+	for (int i = 0; i < BITS; ++i) {
 	//for (int i = 0; i < 32 && a != 1; ++i) {
 		a *= a;
 		b *= a;
@@ -40,8 +41,23 @@ u32 mmi_euler(u32 n) {
 	return b;
 }
 
+// https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
 u32 mmi_exteucl(u32 n) {
-	return 0;
+	u32 rowa[4], rowb[4]; // qrst
+	rowa[1] = 1 << (BITS-1); rowa[2] = 1; rowa[3] = 0;
+	rowb[1] =             n; rowb[2] = 0; rowb[3] = 1;
+	u32* rowx = rowa, *rowy = rowb;
+	while (rowx[1] && rowy[1]) {
+		u32 q = rowx[1] / rowy[1];
+		u32 r = rowx[1] % rowy[1];
+		u32 s = rowx[2] - q * rowy[2];
+		u32 t = rowx[3] - q * rowy[3];
+		rowx[0] = q; rowx[1] = r; rowx[2] = s; rowx[3] = t;
+		u32* h = rowx;
+		rowx = rowy;
+		rowy = h;
+	}
+	return rowx[3];
 }
 
 void test_val(u32 n) {
@@ -70,6 +86,7 @@ int main(int argc, char** argv) {
 		TEST_FUNC(mmi_mnv_noif, "mnv noif")
 		TEST_FUNC(mmi_mnv_if, "mnv if")
 		TEST_FUNC(mmi_euler, "euler")
+		TEST_FUNC(mmi_exteucl, "extended euclidian")
 	}
 
 	return 0;
