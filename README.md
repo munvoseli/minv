7ba65adb6b07ceda2ef21949093487da99fb0715
munvoseli <muslinmelody@gmail.com>
2022-08-02 15:18:12 -0400

    made another algorithm

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 comparison of multiplicative inverses for odd numbers in mod 2<sup>n</sup>
 
-The mnv algorithms are a thing.
+The mnv if and noif algorithms are similar to each other.  It's just that one does things without the branch.
+
+The mnv boi algorithm tried.
 
 I assume the Euler algorithm is how it's implemented elsewhere, but I did re-create it from Euler's theorem with the totient function.
 
@@ -9,3 +11,5 @@ There are two Extended Euclidian algorithms present.  Both are from Wikipedia, a
 <hr/>
 
 For the Euler algorithm, there are multiple looping options.  They should all produce identical results (for odd-number inputs), but the "normal" for loop (whose condition is `i < 32`) was fastest on my machine.
+
+i forget if i reaches 32 normally at all
--- speed.c
+++ speed.c
@@ -1,11 +1,55 @@
 #include <stdio.h>
 #include <time.h>
 
-typedef unsigned int u32;
 #define TRIALS 10000000
+#define B64
+
+#ifdef B64
+typedef unsigned long u32;
+#define BITS 64
+#endif
+#ifdef B32
+typedef unsigned int u32;
 #define BITS 32
+#endif
+
+u32 mmi_mnv_boi(u32 n) {
+	u32 m = 1;
+	u32 j = 1;
+	for (int i = 0; i < BITS; ++i) {
+		j <<= 1;
+		if (((m * n) & (2 * j - 1)) != 1) {
+			m |= j;
+		}
+	}
+	return m;
+}
 
 u32 mmi_mnv_noif(u32 n) {
+#ifdef B64
+	// i think the original code was faster,
+	// but after various attempts to optimize
+	// and fix things for 64 bits
+	// things happened
+	u32 s = n;
+	u32 h[2] = {0, n};
+	unsigned int m0 = 1;
+	for (unsigned int i = 1; i < 32; ++i) {
+		s >>= 1;
+		unsigned int a = s & 1;
+		s += h[a];
+		m0 |= a << i;
+	}
+	unsigned int m1 = 0;
+	for (unsigned int i = 0; i < 32; ++i) {
+		s >>= 1;
+		unsigned int a = s & 1;
+		s += h[a];
+		m1 |= a << i;
+	}
+	return (((u32) m1) << 32) | m0;
+#endif
+#ifdef B32
 	u32 s = n;
 	u32 m = 1;
 	for (int i = 1; i < BITS; ++i) {
@@ -14,13 +58,14 @@ u32 mmi_mnv_noif(u32 n) {
 		m |= a;
 	}
 	return m;
+#endif
 }
 
 u32 mmi_mnv_if(u32 n) {
-	int s = n;
-	int m = 1;
+	u32 s = n;
+	u32 m = 1;
 	for (int i = 1; i < BITS; ++i) {
-		int a = s & (1 << i);
+		u32 a = s & (((u32) 1) << i);
 		if (a) {
 			s += n << i;
 			m |= a;
@@ -44,7 +89,7 @@ u32 mmi_euler(u32 n) {
 // https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
 u32 mmi_exteucl(u32 n) {
 	u32 rowa[2], rowb[2]; // qrst
-	rowa[0] = 1 << (BITS-1); rowa[1] = 0;
+	rowa[0] = ((u32) 1) << (BITS-1); rowa[1] = 0;
 	rowb[0] =             n; rowb[1] = 1;
 	u32* rowx = rowa, *rowy = rowb;
 	while (rowy[0]) {
@@ -61,7 +106,7 @@ u32 mmi_exteucl(u32 n) {
 // https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Simple_algebraic_field_extensions
 u32 mmi_eucl2(u32 n) {
 	u32 t2 = 0; u32 t1 = 1;
-	u32 r2 = 1 << (BITS-1);
+	u32 r2 = ((u32) 1) << (BITS-1);
 	u32 r1 = n;
 	while (r1) {
 		u32 q = r2 / r1;
@@ -74,11 +119,22 @@ u32 mmi_eucl2(u32 n) {
 }
 
 void test_val(u32 n) {
+#ifdef B64
+	printf("%16lx mnv noif\n", mmi_mnv_noif(n));
+	printf("%16lx mnv if\n", mmi_mnv_if(n));
+	printf("%16lx mnv boi\n", mmi_mnv_boi(n));
+	printf("%16lx extended euclidian\n", mmi_exteucl(n));
+	printf("%16lx extended euclidian 2\n", mmi_eucl2(n));
+	printf("%16lx euler\n", mmi_euler(n));
+#endif
+#ifdef B32
 	printf("%8x mnv noif\n", mmi_mnv_noif(n));
 	printf("%8x mnv if\n", mmi_mnv_if(n));
-	printf("%8x euler\n", mmi_euler(n));
+	printf("%8x mnv boi\n", mmi_mnv_boi(n));
 	printf("%8x extended euclidian\n", mmi_exteucl(n));
 	printf("%8x extended euclidian 2\n", mmi_eucl2(n));
+	printf("%8x euler\n", mmi_euler(n));
+#endif
 }
 
 #define TEST_FUNC(func, str) start = clock();\
@@ -88,6 +144,9 @@ printf("%d ms " str "\n", (clock() - start) * 1000 / CLOCKS_PER_SEC);
 
 int main(int argc, char** argv) {
 	clock_t start;
+
+	printf("%d bits\n", sizeof(u32) * 8);
+
 	test_val(13);
 	test_val(15);
 	test_val(4095);
@@ -98,9 +157,10 @@ int main(int argc, char** argv) {
 
 	for (int j = 0; j < 1; ++j) {
 		TEST_FUNC(mmi_mnv_if, "mnv if")
+		TEST_FUNC(mmi_mnv_noif, "mnv noif")
+		TEST_FUNC(mmi_mnv_boi, "mnv boi")
 		TEST_FUNC(mmi_exteucl, "extended euclidian")
 		TEST_FUNC(mmi_eucl2, "extended euclidian 2")
-		TEST_FUNC(mmi_mnv_noif, "mnv noif")
 		TEST_FUNC(mmi_euler, "euler")
 	}
 
